18ac1b1ae9ec56cbc6bffbb3668ca4b219296a33
Antonio Seprano <antonio.seprano@docebo.com>
2020-03-27 17:45:48 +0100

    Fixed Queue interface

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
-// Type definitions for @darkbyte/aqueue 1.0.4
+// Type definitions for @darkbyte/aqueue 1.1.0
 // Project: https://github.com/aseprano/aqueue
 // Definitions by: Antonio Seprano <https://github.com/aseprano>
 
 declare class Queue<T> {
 
-    push(): void;
+    push(newItem: T): void;
 
     pop(): Promise<T>;
 
